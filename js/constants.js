3ba87b1f0cf508e35f7bf26a22b9f17a2ba33464
Nahush Farkande <john@fortella.ai>
2022-02-18 19:46:34 -0500

    With and without mouse constants

--- js/constants.js
+++ js/constants.js
@@ -11,7 +11,16 @@ export const RECORDING_OFF_STATE = {
     timestamp: null
 };
 
-export const FITTS_CONSTANT = {
-    A: 595.8773979024193,
-    B: 144.3519845199607
+// export const FITTS_CONSTANT = {
+// A: 0.5958773979024193,
+// B: 0.1443519845199607
+// };
+const WITH_MOUSE = {
+    A: 0.1065124396143242,
+    B: 0.22043812897329967
 };
+const WITHOUT_MOUSE = {
+    A: 0.41790656467960263,
+    B: 0.1789738911981494
+};
+export const FITTS_CONSTANT = WITH_MOUSE;
