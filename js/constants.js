088b4103899d94d103b3ef1636fc600c79ccf991
Karl F. MacDorman <14225248+macdorman@users.noreply.github.com>
2022-06-17 20:23:47 -0400

    Add files via upload

--- js/constants.js
+++ js/constants.js
@@ -21,19 +21,10 @@ export const RECORDING_OFF_STATE = {
     timestamp: null
 };
 
-// export const FITTS_CONSTANT = {
-// A: 0.5958773979024193,
-// B: 0.1443519845199607
-// };
-const WITH_MOUSE = {
+export const FITTS_CONSTANT = {
     A: 350,
     B: 150
 };
-const WITHOUT_MOUSE = {
-    A: 0.41790656467960263,
-    B: 0.1789738911981494
-};
-export const FITTS_CONSTANT = WITH_MOUSE;
 
 export const THINK_RECORD = { eventType: EVENT_TYPES.THINK, expertTime: THINK_TIME };
 export const SYSTEM_RESPONSE_RECORD = { eventType: EVENT_TYPES.RESPONSE, expertTime: SYSTEM_RESPONSE_TIME };
